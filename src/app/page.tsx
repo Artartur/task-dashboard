998ecf3e016a9d418f16b0ad69d74b73dbe3a47c
artartur <artur.souza@tds.company>
2025-10-02 02:32:39 -0300

    feat(pages): navigate to /login route when the user isn't authenticated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+import { useAuth } from "./hooks/useAuth";
+import Loading from "./components/Loading";
+
 export default function Home() {
+  const { state } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!state.loading) {
+      if (state.isAuthenticated) {
+        router.push("/dashboard");
+      } else {
+        router.push("/login");
+      }
+    }
+  }, [state.isAuthenticated, state.loading, router]);
+
   return (
-    <div>
-      Hello World
-    </div>
+    <Loading />
   );
 }
