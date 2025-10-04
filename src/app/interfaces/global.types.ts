7241e4132bd0cc535759e8bf374d1c2e60ba25a8
artartur <artur.souza@tds.company>
2025-10-04 04:22:18 -0300

    feat(interfaces): add new states and action to global types and define id as optional in the task interface

--- src/app/interfaces/global.types.ts
+++ src/app/interfaces/global.types.ts
@@ -6,15 +6,23 @@ export interface IGlobal {
 }
 
 export interface IGlobalActions {
-  setShowCreateTaskModal: (showCreateTaskModal: boolean) => void;
   createTask: (task: ITask) => void;
+  editTask: (taskId: string, updatedTask: ITask) => void;
+  setSelectedTask: (task: ITask | null) => void;
+  setShowCreateTaskModal: (showCreateTaskModal: boolean) => void;
+  setShowEditTaskModal: (showEditTaskModal: boolean) => void;
 }
 
 export interface IGlobalState {
   showCreateTaskModal: boolean;
+  showEditTaskModal: boolean;
+  selectedTask: ITask | null;
   tasks: ITask[];
 }
 
 export type IGlobalReducerActions =
-  | { type: 'SET_SHOW_CREATE_TASK_MODAL'; payload: boolean }
   | { type: 'CREATE_TASK'; payload: ITask }
+  | { type: 'EDIT_TASK'; payload: { taskId: string; updatedTask: ITask } }
+  | { type: 'SET_SELECTED_TASK'; payload: ITask | null }
+  | { type: 'SET_SHOW_CREATE_TASK_MODAL'; payload: boolean }
+  | { type: 'SET_SHOW_EDIT_TASK_MODAL'; payload: boolean }
--- src/app/interfaces/task.types.ts
+++ src/app/interfaces/task.types.ts
@@ -3,6 +3,7 @@ import { Priority, Status } from "@/validators/createTaskSchema";
 export interface ITask {
   createdBy?: Date;
   description: string;
+  id?: string;
   title: string;
   priority: Priority;
   status: Status;
